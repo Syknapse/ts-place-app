707f4767d86c89da21b9ae4254564265bf9e1430
Syknapse <29199184+Syknapse@users.noreply.github.com>
2022-08-05 10:51:41 +0200

    Enhance style and functionalitiy

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ import { ITodo } from './todo.model'
 const App: React.FC = () => {
   const [todos, setTodos] = useState<ITodo[]>([])
 
-  const addTodoHandler = (text: string) => setTodos(prevTodos => [...prevTodos, { id: Math.random().toString(), text }])
+  const addTodoHandler = (text: string) =>
+    text && setTodos(prevTodos => [...prevTodos, { id: Math.random().toString(), text }])
   const deleteTodoHandler = (id: string) => setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id))
 
   return (
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,4 +1,5 @@
 import React, { useRef } from 'react'
+import './NewTodo.css'
 
 type NewTodoProps = {
   onAddTodo: (text: string) => void
@@ -6,14 +7,16 @@ type NewTodoProps = {
 
 const NewTodo: React.FC<NewTodoProps> = ({ onAddTodo }) => {
   const ref = useRef<HTMLInputElement>(null)
+
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault()
     const value = ref.current!.value
     onAddTodo(value)
+    ref.current!.value = ''
   }
   return (
     <form onSubmit={handleSubmit}>
-      <div>
+      <div className="form-control">
         <label htmlFor="todo-text">Todo Text</label>
         <input type="text" id="todo-text" ref={ref}></input>
       </div>
